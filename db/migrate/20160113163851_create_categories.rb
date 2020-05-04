8aa65ea32166ac0bcabee09c05b3640e6be863fb
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-05-04 01:04:02 +0000

    Automatically backed up by Learn

--- db/migrate/20160113163851_create_categories.rb
+++ db/migrate/20160113163851_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
       t.string :name
-      t.string   "post_status"
+
       t.timestamps null: false
     end
   end
